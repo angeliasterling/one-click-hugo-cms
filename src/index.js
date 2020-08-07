36537a9a0152d0bca026fea514ec33ba9f95ccbb
Angelia Sterling <>
2020-08-07 18:05:52 -0400

    mobile nav

--- src/index.js
+++ src/index.js
@@ -3,4 +3,19 @@
 import "./css/main.css";
 
 // Say hello
-console.log("🦊 Hello! Edit me in src/index.js");
+console.log("Welcome \u03B8's");
+
+document.querySelector('.hamburger').addEventListener('click', _onHamburgerClick);
+
+
+// listener methods
+_onHamburgerClick() {
+	var mobileNav = document.querySelector('.mobile-nav');
+	if (mobileNav.classList.contains('mobile-active')) {
+		mobileNav.classList.remove('mobile-active');
+		mobileNav.classList.add('mobile-inactive');
+	} else {
+		mobileNav.classList.remove('mobile-inactive');
+		mobileNav.classList.add('mobile-active');
+	}
+}
